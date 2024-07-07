38d0d91d5db9ae2270ebcc59eeb26d58101d9894
Vicky K <devmrvicky@gmail.com>
2024-07-07 10:10:22 +0530

    receive chats bug fix

--- client/src/hooks/chat/useListMessages.js
+++ client/src/hooks/chat/useListMessages.js
@@ -2,22 +2,34 @@ import { useEffect } from "react";
 import { useSocketContext } from "../../context/SocketContext";
 import useStore from "../../zustand/store";
 import receiveMsgSound from "../../assets/sound/receive-msg.mp3";
+import { useAuthContext } from "../../context/AuthContext";
 
 const useListMessages = () => {
   const { socket } = useSocketContext();
-  const { addMessage } = useStore((store) => store);
+  const { addMessage, selectedFriend } = useStore((store) => store);
+
+  const { authUser } = useAuthContext();
 
   useEffect(() => {
     socket?.on("newMessage", async ({ message }) => {
-      addMessage(message);
-      const audio = new Audio(receiveMsgSound);
-      await audio.play();
+      console.log(
+        { senderId: message.senderId },
+        { receiverId: message.receiverId },
+        { id: selectedFriend?._id },
+        message.senderId === selectedFriend?._id
+      );
+      if (message.senderId === selectedFriend?._id) {
+        console.log(message);
+        addMessage(message);
+        const audio = new Audio(receiveMsgSound);
+        await audio.play();
+      }
     });
 
     return () => {
       socket?.off("newMessage");
     };
-  }, [socket]);
+  }, [socket, selectedFriend?._id]);
 };
 
 export default useListMessages;
